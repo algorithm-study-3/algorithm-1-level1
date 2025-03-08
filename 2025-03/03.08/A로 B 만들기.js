f26d5d2f3d0823995eb9d184b9df085df4b21c2c
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-08 15:09:06 +0900

    정렬, 해시 / 빈도수 카운팅: A로 B 만들기

---
+++ 2025-03/03.08/A로 B 만들기.js
@@ -0,0 +1,8 @@
+function solution(before, after) {
+  var answer = 0;
+  answer =
+    before.split('').sort().join('') === after.split('').sort().join('')
+      ? 1
+      : 0;
+  return answer;
+}
