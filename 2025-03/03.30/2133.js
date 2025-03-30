f9ad20f35aeac550c04ccbe4eaf34dec7cf5859d
minji2219 <aabb0219@naver.com>
2025-03-30 23:46:38 +0900

    25.03.30

--- 2025-03/03.30/2133.js
+++ 2025-03/03.30/2133.js
@@ -1,16 +1,18 @@
 const n = parseInt(require('fs').readFileSync('input.txt').toString().trim());
 
-dp = [];
-dp[0] = 0;
-dp[1] = 0;
-dp[2] = 3;
+if (n % 2 === 1) console.log(0);
+else {
+  dp = [];
+  dp[0] = 1;
+  dp[1] = 0;
+  dp[2] = 3;
 
-for (let i = 3; i <= n; i++) {
-  if (i % 2 === 1) dp[i] = 0;
-  dp[i] = dp[i - 2] * dp[2];
-  for (let j = i - 4; j >= 0; j -= 2) {
-    dp[i] += dp[j] * 2;
+  for (let i = 4; i <= n; i++) {
+    dp[i] = dp[i - 2] * dp[2];
+    for (let j = i - 4; j >= 0; j -= 2) {
+      dp[i] += dp[j] * 2;
+    }
   }
-}
 
-console.log(dp[n]);
+  console.log(dp[n]);
+}
